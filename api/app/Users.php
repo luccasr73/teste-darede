85e90492c384aad0b8b88182e339bdcec66e20b1
luccasr73 <luccasrobert@hotmail.com>
2020-06-21 03:27:41 -0300

    Funções para lidar com o JWT

--- api/app/Users.php
+++ api/app/Users.php
@@ -2,9 +2,24 @@
 
 namespace App;
 
+use Illuminate\Auth\Authenticatable;
+use Laravel\Lumen\Auth\Authorizable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class Users extends Model
+class Users extends Model implements AuthenticatableContract, AuthorizableContract, JWTSubject
 {
-    //
+    use Authenticatable, Authorizable;
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
