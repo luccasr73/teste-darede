99718eef0d4eeed79e4c257c12a9d947d81c51ac
luccasr73 <luccasrobert@hotmail.com>
2020-06-21 03:44:52 -0300

    Correção de texto

--- api/app/Http/Controllers/Controller.php
+++ api/app/Http/Controllers/Controller.php
@@ -6,7 +6,7 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Auth;
 class Controller extends BaseController
 {
-    protected function respondWithToken($token)
+    protected function responseWithToken($token)
     {
         return response()->json([
             'token' => $token,
