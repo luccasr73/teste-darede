d1524df6e60336788647fed4bc8a9225493c4b28
luccasr73 <luccasrobert@hotmail.com>
2020-06-21 05:27:28 -0300

    Testes comuns

---
+++ api/tests/Tests.php
@@ -0,0 +1,21 @@
+<?php
+
+
+class Tests extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    
+    public function testVersion()
+    {
+        $this->json('GET', '/api/versao')
+             ->seeJsonEquals([
+                 'versao' => '1',
+        ]);
+    }
+
+    
+}
