bfaaa407a20cb64bcdc3ad8e9e34bc4093b0bb13
luccasr73 <luccasrobert@hotmail.com>
2020-06-22 02:08:30 -0300

    View Home

--- app/src/components/Home.vue
+++ app/src/components/Home.vue
@@ -1,151 +1,55 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
-
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
-        </p>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
+    <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+            <v-card class="mx-auto" outlined :loading="loading">
+                <v-list-item three-line>
+                    <v-list-item-content>
+                        <div class="overline mb-4">Email</div>
+                        <v-list-item-title class="headline mb-1">{{email}}</v-list-item-title>
+                    </v-list-item-content>
+                    <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
+                </v-list-item>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="primary" :loading="loadingLogout" @click="logout">Sair</v-btn>
+                </v-card-actions>
+            </v-card>
         </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
-    </v-row>
-  </v-container>
+    </v-container>
 </template>
 
 <script>
+import axios from 'axios'
+import {
+  API
+} from '../../env'
+import {
+  getUserId, signOut
+} from '../services/auth'
 export default {
   name: 'Home',
-
   data: () => ({
-    ecosystem: [
-      {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader'
-      },
-      {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify'
-      },
-      {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify'
-      }
-    ],
-    importantLinks: [
-      {
-        text: 'Documentation',
-        href: 'https://vuetifyjs.com'
-      },
-      {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com'
-      },
-      {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify'
-      },
-      {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs'
-      },
-      {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify'
-      }
-    ],
-    whatsNext: [
-      {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com/components/api-explorer'
-      },
-      {
-        text: 'Select a layout',
-        href: 'https://vuetifyjs.com/layout/pre-defined'
-      },
-      {
-        text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions'
-      }
-    ]
-  })
+    email: '',
+    loading: true,
+    loadingLogout: false
+  }),
+  methods: {
+    logout () {
+      signOut()
+      this.$router.push('/login')
+    }
+  },
+  mounted: function () {
+    axios.get(API + '/buscar/usuario/id/' + getUserId())
+      .then(res => {
+        const data = res.data.data
+        this.email = data.email
+        this.loading = false
+      })
+    // eslint-disable-next-line handle-callback-err
+      .catch(err => {
+        this.email = 'Erro ao coletar dados'
+      })
+  }
 }
 </script>
